3b0350fbcb18cd9adbb13e7559547e25e6fd814f
ejihuang <ejihuang@outlook.com>
2023-12-07 20:11:32 +0800

    initialized.

---
+++ MediamtxGui/Interop/Interop.WindowSubclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MediamtxGui.Interop
+{
+    public static partial class Interop
+    {
+        public static partial class Comctl32
+        {
+            public delegate IntPtr SUBCLASSPROC(
+              IntPtr hWnd,
+              int msg,
+              IntPtr wParam,
+              IntPtr lParam,
+              UIntPtr uIdSubclass,
+              UIntPtr dwRefData
+            );
+
+            [DllImport(Libraries.Comctl32, ExactSpelling = true)]
+            public static extern bool SetWindowSubclass(
+                IntPtr hWnd,
+                SUBCLASSPROC pfnSubclass,
+                UIntPtr uIdSubclass,
+                UIntPtr dwRefData
+            );
+
+            [DllImport(Libraries.Comctl32, ExactSpelling = true)]
+            public static extern IntPtr DefSubclassProc(
+                IntPtr hWnd,
+                int msg,
+                IntPtr wParam,
+                IntPtr lParam
+            );
+        }
+    }
+}
